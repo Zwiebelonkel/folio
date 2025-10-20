4758538cead587397ff9f38818a4ce35ebfa2151
Luca Müller <lucamuller2004@gmail.com>
2025-10-20 13:46:42 +0000

    turn up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,16 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QCE8WV2Z4S"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QCE8WV2Z4S');
+          `}
+        </script>
       </head>
       <body className="font-body antialiased pt-16">
         {children}
