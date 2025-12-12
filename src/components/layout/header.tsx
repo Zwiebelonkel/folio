0f26883e5be4cda274922878db1c019b1842f731
Luca Müller <lucamuller2004@gmail.com>
2025-12-12 16:21:56 +0000

    completely left, sorry

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -17,7 +17,8 @@ export function Header() {
       className="fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur-sm dark:bg-black/80"
     >
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
+        <div className="flex h-16 items-center justify-start gap-4">
+          <SidebarTrigger />
           <div onClick={scrollToTop} className="flex items-center gap-3 cursor-pointer">
             <Image 
               src="/images/LM_nobg.png" 
@@ -30,7 +31,6 @@ export function Header() {
               Luca Müller
             </h1>
           </div>
-          <SidebarTrigger />
         </div>
       </div>
     </header>
